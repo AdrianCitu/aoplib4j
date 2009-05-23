ebacbe26790b4c02b0d40f0e904dd0bdf10afc75
citu_adrian@yahoo.com <citu_adrian@yahoo.com@be53a15a-c520-11dd-9d8b-4f985c3f9f46>
2009-05-23 14:12:07 +0000

    Issue 50:Package level modularity aspect - the implementation

---
+++ trunk/aoplib4j/src/test/java/com/google/code/aoplib4j/aspectj/modularity/clas/ChildOfForbiddenCallerClass.java
@@ -0,0 +1,34 @@
+/*
+ *  Copyright 2008-2009 the original author or authors.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.google.code.aoplib4j.aspectj.modularity.clas;
+
+/**
+ * Subclass of the {@link ForbiddenCallerClass}. This class is used by the 
+ * JUnit test to verify the behavior of subclasses in the case of boundary 
+ * violations.
+ * @author Adrian Citu
+ *
+ */
+public class ChildOfForbiddenCallerClass extends ForbiddenCallerClass {
+
+    public void childMethod() {
+        
+    }
+    
+    public static void staticChildVoidMethod() {
+        
+    }    
+    
+}
