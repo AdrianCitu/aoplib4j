9cb6f7d1637c1484faa4f09684c6df27bc89e4ca
citu_adrian <citu_adrian@yahoo.cm>
2009-05-23 14:12:07 +0000

    Issue 50:Package level modularity aspect - the implementation

---
+++ aoplib4j/src/test/java/com/google/code/aoplib4j/aspectj/modularity/clas/ForbiddenCallerClass.java
@@ -0,0 +1,39 @@
+/*
+ *  Copyright 2008-2009 the original author or authors.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.google.code.aoplib4j.aspectj.modularity.clas;
+
+/**
+ * 
+ * @author Adrian Citu
+ * This is a class in which is forbidden to call methods from the ClasCalledClass.
+ * It is used by the junit tests.
+ * 
+ *  @see ClasCalledClass
+ * 
+ *
+ */
+public class ForbiddenCallerClass {
+    
+    private ClasCalledClass called = new ClasCalledClass();
+    
+    public void voidMethod() {
+        called.calledMethod();
+    }
+
+    public static void staticVoidMethod() {
+        ClasCalledClass.calledStaticMethod();
+    }
+    
+}
