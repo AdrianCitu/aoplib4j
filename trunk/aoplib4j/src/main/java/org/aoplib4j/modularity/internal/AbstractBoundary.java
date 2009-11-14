4ef45f52871bc82ee93793cfa1728d68befdc517
citu_adrian@yahoo.com <citu_adrian@yahoo.com@be53a15a-c520-11dd-9d8b-4f985c3f9f46>
2009-11-14 17:44:53 +0000

    Issue 61:change the package names from com.code.google.aoplib4j to org.aoplib4j Change containing only the sources.

---
+++ trunk/aoplib4j/src/main/java/org/aoplib4j/modularity/internal/AbstractBoundary.java
@@ -0,0 +1,248 @@
+/*
+ *  Copyright 2008-2009 the original author or authors.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.aoplib4j.modularity.internal;
+
+import java.lang.reflect.Method;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.aoplib4j.modularity.BoundaryViolationCallback;
+import org.aoplib4j.modularity.ClassBoundary;
+import org.aoplib4j.modularity.ListType;
+import org.aoplib4j.modularity.PackageBoundary;
+import org.aoplib4j.modularity.ViolationInformation;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+
+
+/**
+ * Abstract class containing the common behavior for the package modularity
+ * and class modularity aspects.
+ * 
+ * @author Adrian Citu
+ *
+ */
+abstract class AbstractBoundary {
+
+    /**
+     * the logger to use.
+     */
+    private static final Logger LOGGER = Logger
+            .getLogger(AbstractBoundary.class.getName());
+    
+    /**
+     * 
+     * Method that will create  a callback class using information from the
+     * {@link ClassBoundary} or {@link PackageBoundary} and execute the 
+     * callback by calling 
+     * {@link BoundaryViolationCallback#boundaryViolation(ViolationInformation)}
+     * .
+     * 
+     * @param calledClass the class of called object.
+     * @param classBoundary the class boundary annotation (if applicable)
+     * @param pkgBoundary the package boundary annotation (if applicable)
+     * @param calledMethod the called method
+     * @param callerSte the caller stacke trace.
+     */
+     void createAndExecuteCallback(final Class< ? > calledClass,
+            final ClassBoundary classBoundary, 
+            final PackageBoundary pkgBoundary,
+            final Method calledMethod,
+            final StackTraceElement callerSte) {
+        try {
+            BoundaryViolationCallback cllbck = 
+                createCallBackInstance(classBoundary, pkgBoundary);
+
+            ViolationInformation bv = new ViolationInformationImpl(
+                    calledMethod.getName(), 
+                    calledClass.getCanonicalName(),
+                    callerSte.getClassName());
+
+            cllbck.boundaryViolation(bv);
+        } catch (InstantiationException e) {
+            LOGGER.log(Level.WARNING, "Creation of call back failed", e);
+        } catch (IllegalAccessException e) {
+            LOGGER.log(Level.WARNING, "Creation of call back failed", e);
+        }
+    }
+     
+     /**
+      * Creates an instance of boundary violation callback using the information
+      * from the {@link ClassBoundary#callbackClass()} or from the
+      * {@link PackageBoundary#callbackClass()}.
+      * 
+      * 
+      * @see BoundaryViolationCallback
+      * 
+      * @param classBoundary
+      *            the {@link ClassBoundary} annotation.
+      * @param pkgBoundary the {@link PackageBoundary} annotation.
+      * 
+      * @return instance of a {@link BoundaryViolationCallback}.
+      * 
+      * @throws IllegalAccessException
+      *             if the instantiation fails (introspection is used)
+      * @throws InstantiationException
+      *             if the instantiation fails (introspection is used)
+      */
+     private BoundaryViolationCallback createCallBackInstance(
+             final ClassBoundary classBoundary,
+             final PackageBoundary pkgBoundary)
+             throws InstantiationException, IllegalAccessException {
+         
+         Class< ? extends BoundaryViolationCallback> callBackClass = null;
+         
+         if (classBoundary == null) {
+             callBackClass = pkgBoundary.callbackClass();
+         } else {
+             callBackClass = classBoundary.callbackClass();
+         }
+             
+
+         LOGGER.info("Creating a boundary callback instance of " 
+                 + callBackClass);
+
+         return callBackClass.newInstance();
+
+     }
+     
+     /**
+      * Method that computes the information about the caller class. The
+      * information is stored into a {@link StackTraceElement} object.
+      * 
+      * The caller information is on the third position on the stack trace;
+      * the first position contains the call to this method, and the 
+      * second one to the
+      * {@link 
+      * ClassBoundaryAspect#callOfStaticClassBoundaryAdvice(
+      * org.aspectj.lang.JoinPoint.StaticPart)}
+      * 
+      *or
+      *{@link
+      *PackageBoundaryAspect#callOfStaticPackageBoundaryAdvice(
+      * org.aspectj.lang.JoinPoint.StaticPart)}
+      * 
+      * This method will return faulty results when a child class calls an
+      * inherited non overridden parent method. In this case the stack trace
+      * contains a call to the parent method directly.
+      * 
+      * <pre>
+      *  Example:
+      *  P - parent class 
+      *  P#method - a method of the parent
+      *  C - child class; it does not override the &quot;method&quot; method
+      *  
+      *  On the stack trace a call to C#method is in fact written as a call to
+      *  P#method.
+      * 
+      * </pre>
+      * 
+      * @param calledClassName
+      *            the canonical name of the called class name
+      * @param calledMethodName
+      *            the name of the called method.
+      * @return {@link StackTraceElement} represented the caller method.
+      */
+      StackTraceElement getCallerInformation(
+             final String calledClassName, final String calledMethodName) {
+
+         StackTraceElement[] stes = new Throwable().getStackTrace();
+
+         return stes[2];
+     }
+
+     /**
+      * Method that will create  a callback class using information from the
+      * {@link ClassBoundary} or {@link PackageBoundary} and execute the 
+      * callback by calling 
+      *{@link BoundaryViolationCallback#boundaryViolation(ViolationInformation)}
+      * .
+      * 
+      * @param calledObj the called object
+      * @param callerObj the caller object
+      * @param jp aspectj joinpoint
+      * @param pkgBoundary package boundary annotation (if applicable)
+      * @param classBoundary class boundary annotation (if applicable)
+      */
+      void createAndExecuteCallback(
+              final Object calledObj,
+              final Object callerObj, 
+              final JoinPoint jp,
+              final PackageBoundary pkgBoundary, 
+              final ClassBoundary classBoundary) {
+          
+          try {
+              BoundaryViolationCallback cllbck = 
+                  createCallBackInstance(classBoundary, pkgBoundary);
+
+              Method calledMethod = ((MethodSignature) jp.getSignature())
+              .getMethod();
+
+              ViolationInformation bv = new ViolationInformationImpl(
+                      calledMethod.getName(), 
+                      calledObj.getClass().getCanonicalName(), 
+                      callerObj.getClass().getCanonicalName());
+
+              cllbck.boundaryViolation(bv);
+          } catch (InstantiationException e) {
+              LOGGER.log(Level.WARNING, "Creation of call back failed", e);
+          } catch (IllegalAccessException e) {
+              LOGGER.log(Level.WARNING, "Creation of call back failed", e);
+          }
+      }
+      /**
+       * @param stringTolook the string to look for
+       * @param listType the type of list
+       * @param stringArray the array of string on which the search should be 
+       * done
+       * @return true if the <code>stringArray</code> is a 
+       * {@link ListType#BLACKLIST} and the 
+       * <code>stringToLook</code> is present into the array. 
+       * false if the <code>stringArray</code> is a 
+       * {@link ListType#WHITELIST} and the 
+       * <code>stringToLook</code> is present into the array. 
+       */
+       boolean searchStringIntoArray(final String stringTolook,
+              final ListType listType, final String[] stringArray) {
+          
+          
+          if (stringTolook == null || stringArray == null || listType == null) {
+              LOGGER.log(Level.WARNING, 
+                      "One of the following parameters was null:"
+                      + " stringToLook:" + stringTolook
+                      + ", listType:" + listType
+                      + ", stringArray:" + stringArray);
+              return false;
+          }
+          
+          if (ListType.BLACKLIST.equals(listType)) {
+              for (String cls : stringArray) {
+                  if (stringTolook.equals(cls)) {
+                      return true;
+                  }
+              }
+              
+              return false;
+          //WHITELIST    
+          } else {
+              for (String cls : stringArray) {
+                  if (stringTolook.equals(cls)) {
+                      return false;
+                  }
+              }   
+              return true;    
+          }
+      }      
+}
---
+++ trunk/aoplib4j/src/main/java/org/aoplib4j/modularity/internal/ClassBoundaryAspect.java
@@ -0,0 +1,254 @@
+/*
+ *  Copyright 2008-2009 the original author or authors.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.aoplib4j.modularity.internal;
+
+import java.lang.reflect.Method;
+
+import org.aoplib4j.modularity.ClassBoundary;
+import org.aoplib4j.modularity.ListType;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+
+
+/**
+ * Aspect implementing the modularity at the class level.
+ * 
+ * @see ClassBoundary
+ *  
+ * @author Adrian Citu
+ *
+ */
+@Aspect
+public final class ClassBoundaryAspect extends AbstractBoundary {
+
+    /**
+     * Pointcut intercepting the call of methods non private and
+     * non static to the classes annotated with the {@link ClassBoundary}
+     * annotations.
+     * 
+     * <pre>
+     *  AspectJ pointcut:
+     *   call(!private !static * 
+     *   (@org.aoplib4j.modularity.ClassBoundary *).*(..)) 
+     *   && target(calledObj) && this(callerObj) && if()
+     * </pre>
+     * 
+     * @param calledObj instance of the called object.
+     * @param callerObj instance of the caller object.
+     * 
+     * @return false if calledObject == callerObject (the call is made on the 
+     * class instance) or if the retrieved {@link ClassBoundary} annotation
+     * from the called class is null.
+     */
+    @Pointcut("call(!private !static * "
+            + "(@org.aoplib4j.modularity.ClassBoundary *)."
+            + "*(..)) " + "&& target(calledObj) && this(callerObj) && if()")
+    public static boolean callOfClassBoundaryPointcut(final Object calledObj,
+            final Object callerObj) {
+
+        //the caller and called are the same instance
+        if (calledObj == callerObj) {
+            return false;
+        }
+        
+        ClassBoundary boundary = getClassBoundaryAnnotation(calledObj
+                .getClass());
+
+        //advice is called on a subclass; the annotation is not inherited by
+        //the subclasses.
+        if (boundary == null) {
+            return false;
+        }
+        
+        return true;
+    }
+
+    /**
+     * Pointcut intercepting the call of methods non private and
+     * static to the classes annotated with the {@link ClassBoundary}
+     * annotations.
+     * 
+     * <pre>
+     *  AspectJ pointcut:
+     *    call(!private static * 
+     *    (@org.aoplib4j.modularity.ClassBoundary *).*(..))
+     *    && if() 
+     * </pre>
+     * 
+     * @param jpsp AspectJ join point static part.
+     * 
+     * @return false if the {@link ClassBoundary} annotation retrieved from
+     * the calledClass is null true otherwise.
+     */
+    @Pointcut("call(!private static * "
+            + "(@org.aoplib4j.modularity.ClassBoundary *)."
+            + "*(..))  && if()")
+    public static boolean callOfStaticClassBoundaryPointcut(
+            final JoinPoint.StaticPart jpsp) {
+
+        Class< ? > calledClass = jpsp.getSignature().getDeclaringType();
+
+        ClassBoundary boundary = getClassBoundaryAnnotation(calledClass);
+
+        //advice is called on a subclass; the annotation is not inherited by
+        //the subclasses.
+        if (boundary == null) {
+            return false;
+        }
+        
+        return true;
+    }
+
+    /**
+     * Advice for the execution of the
+     * {@link #callOfClassBoundaryPointcut(Object, Object)} pointcut.
+     * 
+     * The advice compute the called class informations (using the 
+     * <code>calledObj</code> parameter), the caller class
+     * informations (using the <code>callerObj</code> parameter) and then 
+     * checks if the caller class is present or not into
+     * the list of forbidden classes.
+     * 
+     * If the caller is present into the list of forbidden classes, the
+     * framework creates an instance of 
+     * {@link 
+     * org.aoplib4j.modularity.BoundaryViolationCallback}
+     * (using
+     * as implementation class taken from {@link ClassBoundary#callbackClass()}
+     * ) and call the callback method 
+     * {@link 
+     * org.aoplib4j.modularity.BoundaryViolationCallback
+     * #boundaryViolation(
+     * org.aoplib4j.modularity.ViolationInformation)}
+     * 
+     * @param calledObj the instance of the called object
+     * @param callerObj the instance of the caller object
+     * @param jp the AspectJ join point.
+     */
+    @Before("callOfClassBoundaryPointcut(calledObj, callerObj)")
+    public void callOfClassBoundaryAdvice(final Object calledObj,
+            final Object callerObj, final JoinPoint jp) {
+        
+        ClassBoundary boundary = getClassBoundaryAnnotation(calledObj
+                .getClass());
+
+        if (boundary != null
+                && classBoundaryViolated(
+                callerObj.getClass().getCanonicalName(), boundary)) {
+            createAndExecuteCallback(
+                    calledObj, callerObj, jp, null, boundary);
+        }
+
+    }
+
+    /**
+     * Advice executed before the execution of a static method found on an 
+     * annotated class.
+     * The advice retrieve the called informations using the
+     * {@link org.aspectj.lang.JoinPoint.StaticPart}
+     * object. Since static pointcuts cannot retrieve the caller object
+     * using <code>this</code> pointcut the caller information is computed 
+     * using the 
+     * stack trace of the current execution thread by creating a new
+     * {@link Throwable} object and calling {@link Throwable#getStackTrace()}.
+     * (see {@link #getCallerInformation(String, String)}).
+     * After the caller and called classes are computed a violation check is 
+     * made.
+     * 
+     * In the case of a violation, the framework creates an instance of 
+     * {@link 
+     * org.aoplib4j.modularity.BoundaryViolationCallback} 
+     * (using
+     * as implementation class taken from 
+     * {@link 
+     * org.aoplib4j.modularity.ClassBoundary#callbackClass(
+     * )}) 
+     * and call the callback method
+     * {@link
+     * org.aoplib4j.modularity.BoundaryViolationCallback
+     * #boundaryViolation(
+     * org.aoplib4j.modularity.ViolationInformation)}
+     *
+     * @param jpsp AspectJ join point static part.
+     * 
+     * @see 
+     *   ClassBoundaryAspect#classBoundaryViolated(String, ClassBoundary)
+     * @see  
+     * org.aoplib4j.modularity.BoundaryViolationCallback
+     */
+    @Before("callOfStaticClassBoundaryPointcut(jpsp)")
+    public void callOfStaticClassBoundaryAdvice(
+            final JoinPoint.StaticPart jpsp) {
+
+        Class< ? > calledClass = jpsp.getSignature().getDeclaringType();
+
+        ClassBoundary boundary = getClassBoundaryAnnotation(calledClass);
+
+        Method calledMethod = ((MethodSignature) jpsp.getSignature())
+                .getMethod();
+
+        StackTraceElement callerSte = this.getCallerInformation(calledClass
+                .getCanonicalName(), calledMethod.getName());
+
+        if (classBoundaryViolated(
+                callerSte.getClassName(), boundary)) {
+
+            createAndExecuteCallback(calledClass, boundary, null, calledMethod,
+                    callerSte);
+        }
+    }
+    
+    /**
+     * Method that returns the {@link ClassBoundary} annotation from the object
+     * passed as parameter or null if the class is not annotated.
+     * 
+     * @param calledClass
+     *            the class from which the annotation should be retrieved.
+     * @return attached {@link ClassBoundary} or null.
+     */
+    private static ClassBoundary getClassBoundaryAnnotation(
+            final Class< ? > calledClass) {
+        
+        return calledClass.getAnnotation(ClassBoundary.class);
+    }
+    
+    /**
+     * Verifies the violation of class boundary by retrieving the information
+     * from the {@link ClassBoundary} annotation (the classes list, the
+     * list type) and comparing with the class name passed as parameter.
+     * 
+     * @param classBoundary {@link ClassBoundary} annotation.
+     * @param callerClassName caller class name.
+     * @return true if a boundary violation; false otherwise.
+     */
+    private boolean classBoundaryViolated(final String callerClassName,
+            final ClassBoundary classBoundary) {
+                
+        ListType listType = classBoundary.classesListType();
+        Class< ? >[] classesList = classBoundary.classesList();
+        
+        String[] classesNamesList = new String[classesList.length];
+        
+        for (int i = 0; i < classesList.length; i++) {
+            classesNamesList[i] = classesList[i].getCanonicalName();
+        }
+        
+        return searchStringIntoArray(
+                callerClassName, listType, classesNamesList);
+    }
+}
